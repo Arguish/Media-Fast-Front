ba06fdc2a2400a56bc8aa606bffd6344c7e14917
Airammg <airam.marting@gmail.com>
2023-05-02 16:20:38 +0100

    Basic User Info done

--- src/Pages/UserPage/UserCard/UserCard.jsx
+++ src/Pages/UserPage/UserCard/UserCard.jsx
@@ -4,28 +4,74 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
-import { CardActionArea } from '@mui/material'
-const UserCard = ({user}) => {
+import { Box, CardActionArea, CardHeader } from '@mui/material'
+const UserCard = ({ user }) => {
   return (
     <>
-      <Card sx={{ 
-        width: '50%',
-        margin: '0 auto' }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            image={user.img_url}
-            alt="User avatar."
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {user.nickname}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, earum quo dolorem quos voluptatibus natus, totam, ut cum numquam tempora obcaecati magnam? Voluptatem numquam quisquam hic minima, laudantium veniam iste?
-            </Typography>
+      <Card
+        sx={{
+          margin: '5px',
+          maxWidth: '95vw',
+          bgcolor: '#000000',
+          height: '50%',
+          border: '2px solid #000',
+          boxShadow: 24,
+          p: 4,
+          color: 'primary.main',
+        }}
+      >
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h4"
+            component="div"
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              alignItems: 'baseline',
+              fontSize: '50px',
+              fontWeight: 'light',
+            }}
+          >
+            {user.nickname}
+            <CardMedia
+              component="img"
+              image={user.img_url}
+              alt="User avatar."
+              sx={{
+                width: '25%',
+              }}
+            />
+          </Typography>
+          <CardContent sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'flex-start',
+            color: 'text.primary'
+          }}>
+            <Typography variant="h5"
+            sx={{
+              margin: '2px 0 5px 0',
+              color: 'secondary.main'
+            }}>
+              USER MEDIA
+            </Typography >
+            {user.media.length > 0 ? (
+              user.media.map((el) => {
+                return (
+                  <Typography sx={{
+                    color: 'primary.main'
+                  }} key={el.user_media.mediumId}>
+                    {el.title} | {el.type} {el.categories.length > 0 ? `| ${el.categories[0].category_name}` : false}
+                  </Typography>
+                )
+              })
+            ) : (
+              <Typography>NO MEDIA ADDED YET.</Typography>
+            )}
           </CardContent>
-        </CardActionArea>
+        </CardContent>
       </Card>
     </>
   )
--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getUserMe, getUserMeMedia } from '../../Services/userServices'
+import { getUserMe } from '../../Services/userServices'
 import UserCard from './UserCard/UserCard'
 const UserPage = () => {
   useEffect(() => {
@@ -8,8 +8,6 @@ const UserPage = () => {
 
   const getUserInfo = async () => {
     const user = await getUserMe(localStorage.userId)
-    const userMedia = await getUserMeMedia(localStorage.userId)
-    console.log(userMedia)
     setUser(user)
   }
 
