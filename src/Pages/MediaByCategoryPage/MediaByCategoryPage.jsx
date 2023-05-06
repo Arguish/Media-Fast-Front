c97074ee7b1e4cd8330a8c21d36597fa72664bf5
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-05-06 17:17:52 +0100

    Media operativa otra vez

--- src/Pages/MediaByCategoryPage/MediaByCategoryPage.jsx
+++ src/Pages/MediaByCategoryPage/MediaByCategoryPage.jsx
@@ -1,8 +1,23 @@
-import React, { useEffect } from 'react'
-import { getMediaByCategories, getMediaByCategoriesAndType } from '../../Services/mediaServices'
+import React, { useEffect, useState } from 'react'
+import MediaCard from '../../Components/MediaCard/MediaCard'
+import { getMediaRandom, getMediaByID } from '../../Services/mediaServices'
+
+import {
+  getMediaByCategories,
+  getMediaByCategoriesAndType,
+} from '../../Services/mediaServices'
 import { useParams } from 'react-router-dom'
+
 const MediaByCategoryPage = () => {
-  
+  const [contentReady, setcontentReady] = useState(false)
+  const [loading, setloading] = useState(false)
+  const [tryCount, settryCount] = useState(0)
+  const [spin, setspin] = useState(0)
+  const [list, setList] = useState([{}])
+  const [listItem, setListItem] = useState(0)
+
+  const maxCount = 3
+
   const { type } = useParams()
 
   useEffect(() => {
@@ -14,9 +29,76 @@ const MediaByCategoryPage = () => {
     // const result = await getMediaByCategories(userId)
     const result = await getMediaByCategoriesAndType(userId, type)
     console.log(result)
+    setList(result.slice(0, maxCount))
+    setcontentReady(true)
   }
-
-  return <div>MediaByCategoryPage</div>
+  const next = () => {
+    settryCount(tryCount + 1)
+    setloading(true)
+    setspin(spin + 720)
+    if (listItem >= list.length - 1) {
+      setListItem(0)
+    } else {
+      setListItem(listItem + 1)
+    }
+  }
+  const previous = () => {
+    setspin(spin - 720)
+    if (listItem <= 0) {
+      setListItem(list.length - 1)
+    } else {
+      setListItem(listItem - 1)
+    }
+  }
+  return (
+    <div className="mediaCardWrapper">
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {loading && !contentReady && (
+          <h2>
+            El servidor esta tardando{tryCount > 3 ? ' MUCHO' : ''}... Espere
+            por Favor...
+          </h2>
+        )}
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          {contentReady && <button onClick={previous}>{' <<< '}</button>}
+          <div
+            style={{
+              transition: 'all 200ms',
+              transform: `rotateY(${spin}deg)`,
+            }}
+          >
+            {contentReady && (
+              <MediaCard
+                cover={`https://picsum.photos/seed/${list[listItem]}/200/300`}
+                cardContent={list[listItem]}
+              ></MediaCard>
+            )}
+          </div>
+          {console.log(list[listItem])}
+          <button onClick={next}>
+            {contentReady ? ' >>> ' : 'Estoy listo!'}
+          </button>
+        </div>
+        {contentReady && (
+          <button
+            onClick={() => {
+              getMediaByID(list[listItem].id).then(
+                (A) => (window.location = A.platforms[0].platform_url)
+              )
+            }}
+          >
+            <h2>VER AHORA!</h2>
+          </button>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default MediaByCategoryPage
