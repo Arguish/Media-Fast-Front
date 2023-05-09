c00df723987ce0bc5c914a0cae5ba7f4e359408d
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-05-09 11:49:51 +0100

    intro guay

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { ContextProvider } from './Context/Context/ContextProvider'
 import { Navigate, useNavigate } from 'react-router-dom'
 import useSound from 'use-sound'
+import './app.css'
 
 import chaaaaaanFX from '../src/assets/braam-6150.mp3'
 
@@ -23,20 +24,22 @@ function App() {
   return (
     <>
       <ContextProvider>
-        <iframe
-          style={{
-            borderRadius: '50%',
-            boxShadow: '10px 10px 10px black',
-            border: '10px solid #ee9e09',
-          }}
-          src="https://giphy.com/embed/x9hvUcjOAM0jC"
-          width="480"
-          height="480"
-          className="giphy-embed"
-          allowFullScreen
-        ></iframe>
-
-        {}
+        <div style={{ textAlign: 'center', overflow: 'hidden' }}>
+          <h1 id="MEDIA">MEDIA</h1>
+          <iframe
+            style={{
+              borderRadius: '50%',
+              boxShadow: '10px 10px 10px black',
+              border: '10px solid #ee9e09',
+            }}
+            src="https://giphy.com/embed/x9hvUcjOAM0jC"
+            width="480"
+            height="480"
+            className="giphy-embed"
+            allowFullScreen
+          ></iframe>
+          <h1 id="FAST">FAST</h1>
+        </div>
       </ContextProvider>
     </>
   )
