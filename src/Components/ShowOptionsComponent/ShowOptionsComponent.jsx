1c974bfb88861b2edf8a2be934d5d1cd46142d45
Airammg <airam.marting@gmail.com>
2023-05-18 19:55:40 +0100

    Scaled down font size in ShowOptionsComponent

--- src/Components/ShowOptionsComponent/ShowOptionsComponent.jsx
+++ src/Components/ShowOptionsComponent/ShowOptionsComponent.jsx
@@ -20,7 +20,11 @@ const ShowOptionsComponent = ({ quest, array }) => {
         <CardHeader
           disableTypography={true}
           sx={{
-            fontSize: 'calc(16px + 2vw)',
+            fontSize: {
+              xs: 'calc(16px + 2vw)',
+              sm: 'calc(16px + 1.5vw)',
+              lg: 'calc(16px + 1.2vw)'
+            },
             textAlign: 'center',
             fontWeight: 'bold',
             fontFamily: 'Poppins, sans seriff',
@@ -58,8 +62,9 @@ const customBtn = {
   borderRadius: '15px',
   fontWeight:'normal',
   fontSize: {
-    xs: 'calc(16px + 1.2vw)',
-    sm: 'calc(16px + 1.3vw)'
+    xs: 'calc(16px + 2vw)',
+    sm: 'calc(16px + 1.5vw)',
+    lg: 'calc(16px + 1.2vw)'
   },
   fontFamily: 'Poppins, sans seriff',
   textShadow: '0.1px 0.1px white',
