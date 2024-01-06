c45441e00377a935cf427bbbeaef765738145801
Airammg <108668077+Airammg@users.noreply.github.com>
2024-01-06 00:44:28 +0000

    Update config.js

--- src/Services/config.js
+++ src/Services/config.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://media-fast-app-production.up.railway.app/api',
-  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://mf-api.onrender.com/api',
   headers: { token: localStorage.getItem('token') }
 })
 
